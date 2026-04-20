ee477441310f8669897c393a2d46b1a8ca0f1737
Mario Afonso <marioafonso1997@gmail.com>
2026-04-20 14:41:09 +0100

    first commit

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -2,9 +2,14 @@
 
 print("A-Maze-ing project setup")
 
+
 def main() -> None:
     """Start the program."""
     print("A-Maze-ing project setup")
+
+
+def test() -> None:
+    pass
 
 
 if __name__ == "__main__":

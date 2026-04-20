17be6603e377186956b8cc3a3195bbb21df5e5ab
Alexandre Miguel Prior Dias Das Neves <alemigue@c2r5s7.42porto.com>
2026-04-20 10:44:51 +0100

    20/4 config_parser added and exceptions as well

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -1,1 +1,26 @@
+#Exceptions Errors para o projeto A-Maze-ing
 
+class MazeError(Exception):
+    """Base exception for all maze-related errors."""
+    pass
+class ConfigError(MazeError):
+    """Raised when the configuration file has issues."""
+    pass
+class MazeGenerationError(MazeError):
+    """Raised when maze generation fails (e.g., impossible parameters)."""
+    pass
+class MazeWriteError(MazeError):
+    """Raised when writing the maze output file fails."""
+    pass
+class ConfigFileNotFoundError(ConfigError):
+    """Raised when the configuration file does not exist."""
+    pass
+class ConfigSyntaxError(ConfigError):
+    """Raised when a line in the config file has invalid syntax."""
+    pass
+class ConfigMissingKeyError(ConfigError):
+    """Raised when a mandatory key is missing from the config."""
+    pass
+class ConfigValueError(ConfigError):
+    """Raised when a config value is invalid (wrong type, out of range, etc.)."""
+    pass
